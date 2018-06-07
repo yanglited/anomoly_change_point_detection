e53b30e5ad71b4ded62de3b8458229c7a3bc293b
Yang Li <yang.li@osram.com>
2018-06-07 14:12:13 -0400

    updated.

--- qdetector.py
+++ qdetector.py
@@ -42,31 +42,41 @@ class Qdetector(object):
         Cov_Mat_Est = np.zeros((self.dimensionality, self.dimensionality, n));
         for k in np.arange(n):#assuming the last data point cannot be detected as a change point, since covariance matrix will become a zero matrix.
             Cov_Mat_Est[:,:,k] = (n-(k+1))/((n-1.0)*n*(k+1))*np.matmul(self.Rn_of_Zi, self.Rn_of_Zi.transpose())
-            print(Cov_Mat_Est[:,:,k])
+            # print(Cov_Mat_Est[:,:,k])
             inv_Cov = np.linalg.inv(Cov_Mat_Est[:,:,k]+0.000001*np.eye(self.dimensionality, dtype=np.float64))
             self.Rkn[0,k] = np.matmul( np.matmul(self.Rn_bar_nk[:,k].transpose(), inv_Cov), self.Rn_bar_nk[:,k] )
 
+        RknVar = np.var(self.Rkn, axis=1)
+        RknMean = np.mean(self.Rkn, axis=1)
         self.changePoint = np.argmax(self.Rkn)
-        if self.Rkn[0, self.changePoint] >= self.threshold:
+        print("RknVar", RknVar)
+        print("RknMean", RknMean)
+        print("self.Rkn[0, self.changePoint]", self.Rkn[0, self.changePoint])
+
+        if RknVar/RknMean >= 3.0:
             self.detectionFlag = True
-            print("Change point detected at sample # %d.", self.changePoint)
+            print("Change point detected at sample # %d." % self.changePoint)
+        # if self.Rkn[0, self.changePoint] >= self.threshold:
+        #     self.detectionFlag = True
+        #     print("Change point detected at sample # %d.", self.changePoint)
 
 def main():
     numOfEvents = 2
-    stateVector = np.asarray([1,1,0,1,0,0,1,1,1,0,1,1,1,1,0,0])
-    stateVector = np.asarray([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1])
+    # stateVector = np.asarray([1,1,0,1,0,0,1,1,1,0,1,1,1,1,0,0])
+    # stateVector = np.asarray([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1])
     stateVector = np.asarray([1,0])
     # stateVector = np.random.randint(2, size=numOfEvents)
     sojournTimeVector = np.random.randint(1, high=300, size=numOfEvents)
     groundTruth = np.repeat(stateVector, sojournTimeVector)
     numberOfSamples = np.sum(sojournTimeVector)
 
-    sigma = .5
+    sigma = 1.0
     np.random.seed()
     samples1 = sigma * np.random.randn(groundTruth.size) + groundTruth
     samples2 = sigma * np.random.randn(groundTruth.size) + groundTruth
+    samples3 = sigma * np.random.randn(groundTruth.size) + groundTruth
 
-    samples = np.vstack((samples1, samples2))
+    samples = np.vstack((samples1, samples2, samples3))
     # samples = samples1
     threshold = numberOfSamples/3.0
     detector = Qdetector(30, samples)
